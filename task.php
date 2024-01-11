6b6a0fc33eaa813c2ffced8e066d61c718269fa4
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-01-12 01:33:23 +0600

    complete photo file add

--- task.php
+++ task.php
@@ -2,8 +2,8 @@
 
 // Data submitted
   if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
+    $name = $_POST['email'];
+    $phone = $_POST['password'];
     $photo = $_FILES['photo'];
 
     // Check if data folder exists
@@ -13,14 +13,25 @@
 
     // Check if file exists
     if (!file_exists('data/phonebook.csv')) {
-      $file = fopen('data/phonebook.csv', 'w');
-      $photoName = str_replace(' ', '+', $name);
-      fputcsv($file, [$name, $phone, 'https://ui-avatars.com/api/?name='.$photoName.'?size=128']);
-    }else{
-      $file = fopen('data/phonebook.csv', 'a');
-      $photoName = str_replace(' ', '+', $name);
-      fputcsv($file, [$name, $phone, 'https://ui-avatars.com/api/?name='.$photoName.'?size=128']);
+        $file = fopen('data/phonebook.csv', 'w');
+        $photoName = str_replace(' ', '+', $name);
+        fputcsv($file, [$name, $phone, 'https://ui-avatars.com/api/?name='.$photoName.'?size=128']);
+      }else{
+        $file = fopen('data/phonebook.csv', 'a');
+        $photoName = str_replace(' ', '+', $name);
+        fputcsv($file, [$name, $phone, 'https://ui-avatars.com/api/?name='.$photoName.'?size=128']);
+      }
+
+    // Check if photo folder exists 
+    if (!file_exists('photo')) {
+        mkdir('photo', 0777, true);
     }
+
+    // Check if file exists 
+    // if (!file_exists('photo/photo.csv', '')) {
+    //     # code...
+    // }
+    
   }
 
   // Read data
@@ -31,18 +42,30 @@
   }
 ?>
 
+<div style="justify-content: center;">
 
-<h1>
-    Login
-</h1>
-<form action="process_form.php" method="post">
-    <label for="email">Email:</label><br>
-    <input type="text" name="name"><br>
+    <div style="background-color: #e9e9e9; width: 300px; height: 400px; margin: auto; margin-top: 30px; padding: 20px; border-radius: 20px;">
+        <h1 style= "font-weight: bold; text-align: center;">
+            Login
+        </h1>
+        <form action="#" method="post" enctype='multipart/form-data' style="float: right;">
+            <div style="margin-bottom: 10px">
+                <label>
+                    Email: <br>
+                    <input type="text" name="email">
+                </label>
+            </div>
+            <div style="margin-bottom: 10px">
+                <label>
+                    Password:<br>
+                    <input type="password" name="password">
+                </label>
+            </div>
+            <div style="margin-bottom: 10px">
+                <input type='file' name='photo'/>
+            </div>
+            <input type="submit" name= "submit" value="Submit">
+        </form>
+    </div>
 
-    <label for="password">Password:</label><br>
-    <input type="password" name="email"><br>
-
-    <input type='file' name='photo'/><br>
-
-    <input type="submit" value="Submit">
-  </form>
+</div>
